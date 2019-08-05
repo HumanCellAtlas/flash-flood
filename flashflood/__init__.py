07d6ef42c2e75c36d22ceeec5a2568aeaa4c709b
Brian Hannafious <32105697+xbrianh@users.noreply.github.com>
2019-08-05 15:05:40 -0600

    More meaningful name for event data offset (#17)

--- flashflood/__init__.py
+++ flashflood/__init__.py
@@ -70,7 +70,7 @@ class FlashFlood:
                         from_date=timestamp,
                         to_date=timestamp,
                         size=len(data),
-                        events=[dict(event_id=event_id, timestamp=timestamp, start=0, size=len(data))])
+                        events=[dict(event_id=event_id, timestamp=timestamp, offset=0, size=len(data))])
         self._upload_journal(_Journal(journal_id, manifest, io.BytesIO(data)), is_new=True)
         return Event(event_id, date, data)
 
@@ -80,7 +80,7 @@ class FlashFlood:
         combined_data = b""
         for journal in self._get_new_journals(number_of_events):
             for i in journal.manifest['events']:
-                events.append({**i, **dict(start=len(combined_data))})
+                events.append({**i, **dict(offset=len(combined_data))})
             combined_data += journal.body.read()
             journals_to_delete.append(journal.id_)
         blob_id = str(uuid4())
@@ -122,7 +122,7 @@ class FlashFlood:
     def get_event(self, event_id):
         journal_id, manifest, item = self._lookup_event(event_id)
         blob_key = f"{self._blobs_pfx}/{journal_id.blob_id}"
-        byte_range = f"bytes={item['start']}-{item['start'] + item['size'] - 1}"
+        byte_range = f"bytes={item['offset']}-{item['offset'] + item['size'] - 1}"
         data = self.bucket.Object(blob_key).get(Range=byte_range)['Body'].read()
         return Event(event_id, datetime_from_timestamp(item['timestamp']), data)
 
@@ -130,13 +130,13 @@ class FlashFlood:
         journal_id, manifest, item = self._lookup_event(event_id)
         blob_key = f"{self._blobs_pfx}/{journal_id.blob_id}"
         blob_data = self.bucket.Object(blob_key).get()['Body'].read()
-        new_blob_data = (blob_data[:item['start']]
+        new_blob_data = (blob_data[:item['offset']]
                          + new_event_data
-                         + blob_data[item['start'] + item['size']:])
+                         + blob_data[item['offset'] + item['size']:])
         item['size'] = len(new_event_data)
         data_size = 0
         for item in manifest['events']:
-            item['start'] = data_size
+            item['offset'] = data_size
             data_size += item['size']
         events = manifest['events']
         manifest = dict(journal_id=journal_id,
@@ -238,7 +238,7 @@ def replay_with_urls(url_info, from_date=None, to_date=None):
         for event_info in manifest['events']:
             if datetime_from_timestamp(event_info['timestamp']) in search_range:
                 break
-        byte_range = f"bytes={event_info['start']}-{manifest['size']-1}"
+        byte_range = f"bytes={event_info['offset']}-{manifest['size']-1}"
         resp = requests.get(urls['events'], headers=dict(Range=byte_range), stream=True)
         resp.raise_for_status()
         for item in manifest['events']:
